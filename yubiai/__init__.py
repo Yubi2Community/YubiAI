62c3a6fc545a6510843f2aef259062d5f44b4f79
venkata-ramireddy-mettu <venkata.ramireddy@credavenue.com>
2023-01-30 17:37:53 +0530

    added subscripting for nlp.

--- yubiai/__init__.py
+++ yubiai/__init__.py
@@ -1,3 +1,6 @@
+"""YubiAi, build great Models. Easy to use."""
+__version__ = "01.23"
+
 ### FTP Server constants
 FTPPORT = "8989"
 FTPHOST = "13.235.92.51"
@@ -6,4 +9,6 @@ FTPHOST = "13.235.92.51"
 import pathlib
 #BASE_PATH = pathlib.Path(__file__).parent.absolute()
 BASE_PATH = pathlib.Path.home().joinpath(".cache", "yubiai").absolute()
+
+
 
